1a1707be1734e805018a2a246e11ee33ced4b599
WalterGuerra <wsteven00@gmail.com>
2024-01-07 17:13:44 -0500

    add hobby model

---
+++ models/hobby.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const hobbySchema = new Schema({
+  title: {
+    type: String, 
+    required: true
+  },
+  type: {
+    type: String,
+    enum: ["work", "personal", "interpersonal"],
+    default: "work",
+  },
+  weeklyGoal: {
+    type: Number,
+    required: true
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "Profile"
+  },
+  currentStreak: {
+    type: Number,
+    default: 0,
+  },
+  LongestStreak: {
+    type: Number,
+    default: 0,
+  },
+  LastCompleted: {
+    type: Date
+  }
+},
+  { timestamps: true }
+)
+
+const Hobby = mongoose.model('Hobby', hobbySchema)
+
+export { Hobby }
