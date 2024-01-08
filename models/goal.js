ce400614f6bb46ed166ed497c070e92655d6669b
WalterGuerra <wsteven00@gmail.com>
2024-01-08 12:00:15 -0500

    add goal model

---
+++ models/goal.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const goalSchema = new Schema({
+  description: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    enum: ["work", "personal", "interpersonal"],
+    default: "personal"
+  },
+  dueDate: {
+    type: String,
+    required: true
+  },
+  priority: {
+    type: String,
+    enum: ["casual", "kind of important", "super important" ],
+    default: "casual"
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "Profile"
+  }
+},
+  { timestamps: true }
+)
+
+const Goal = mongoose.model('Goal', goalSchema)
+
+export { Goal }
