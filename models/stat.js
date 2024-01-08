e5445e40fbcb0202fbe3c402efb14f77924990b8
WalterGuerra <wsteven00@gmail.com>
2024-01-08 12:19:48 -0500

    add stat model

---
+++ models/stat.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const statSchema = new Schema({
+  goalsCompleted: {
+    type: Number,
+    default: 0
+  },
+  hobbiesStreak: {
+    type: Number,
+    default: 0
+  },
+  toDoListStreak: {
+    type: Number,
+    default: 0
+  },
+  toDoListCompleted: {
+    type: Number,
+    default: 0
+  },
+  profile: {
+    type: Schema.Types.ObjectId,
+    ref: 'profile'
+  }
+},
+  {timestamps: true}
+)
+
+const Stat = mongoose.model('Stat', statSchema)
+
+export { Stat }
